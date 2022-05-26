c1f35cc03029eed874c3c49be3471dfa1601793f
wiya-sijabat <wahidismailyusufandesta@gmail.com>
2022-05-26 19:58:23 +0700

    upload

--- app/Views/templet/template_frontend.php
+++ app/Views/templet/template_frontend.php
@@ -137,7 +137,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <footer class="main-footer">
       <!-- To the right -->
       <div class="float-right d-none d-sm-inline">
-        Anything you want
+        Rumah Gemilang Indonesia
       </div>
       <!-- Default to the left -->
       <strong>Copyright &copy; <?= date('Y') ?><a href="<?= $setting['web'] ?>"><?= $setting['nama_sekolah'] ?></a>.</strong> All rights reserved.
